ecc44870990fba49994dbdcc919efabf655633bc
ryan <ryan@verdascend.com>
2018-09-18 14:52:37 -0700

    seqs2ftab first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+seqs2ftab
+========
+
+### Sequence to Feature Table tool
+
+9/18/18 RTK V0.1
+
+Overview
+
+The script seqs2ftab.py is used, in conjunction with "score definition" 
+and sequence files, to generate sequence feature tables suitable for data
+analysis.
+
+
+----------------------------------------------------------------------------
+Dependencies
+
+The script requires pandas.
+
+It also requires a utility function in (included) 'rtk_io.py'
+
+
+----------------------------------------------------------------------------
+Background
+
+The code parses the score definition file, then issues command line calls
+for each definition line and parses output(s) into a table. Each score 
+definition line includes a command line specificiation, with program name
+and options separated by commas (rather than spaces), followed by one or 
+more feature specifications (separated from the command specificiation and
+each other by spaces). When actually called, the command line is given a
+sequence filename as an additional agrument. Feature specifications take 
+the form 'name=col' where the 'name' is a user-defined label for the 
+feature (should be unique), and 'col' indicates which (command line) output 
+column the feature is taken from.
+
+Here is an example score definition line:
+
+    tm_util,-con,1e-7,-sal,0.2,-tmpey,-the  2=tmPey0_f 3=dGPey0_f 4=dHPey0_f 5=dSPey0_f
+
+In this case, the script will call the program tm_util to get thermodynamic
+features. The command line specification and corresponding command line are
+as follows (using 'rand_seqs.dna' as the sequence filename):
+
+    tm_util,-con,1e-7,-sal,0.2,-tmpey,-the
+
+    tm_util -con 1e-7 -sal 0.2 -tmpey -the rand_seqs.dna
+
+The command line options -con and -sal set concentration and salt (to 1e-7 
+and 0.2 M, respectively). The -tmpey and -the options specify the Peyret 
+thermodynamics algorithm and to output thermodynamic values, respectively.
+
+There are four features indicated in the score definition line; From col 2
+we will get sequence melting temperature, Tm (named as 'tmPey0_f'), and from
+columns 3,4,5 we will get free energy ('dGPey0_f'), enthalpy ('dHPey0_f'), 
+and entropy (dSPey0_f).
+
+
+----------------------------------------------------------------------------
+Usage
+
+The script requires a score definition file and a sequence file.
+
+Calling the script with --help will list options and (currently minimal!)
+usage instructions.
+
+The following command line generates a table of features for the given 
+sequences; The output table has 32 features for 1000 sequences.
+
+    ./seqs2ftab.py -d exam_score_defs.txt -s rand_seqs.dna -o rand_seqs.sco 
+
---
+++ seqs2ftab.py
@@ -0,0 +1,334 @@
+#!/usr/bin/env python
+#
+# Sequences to Feature Table
+# Replacement for run_scores.csh
+#
+# Take a sequence file and file defining (the command line for) how to 
+#   calculate various features, then build a feature table with all the 
+#   calculated values for each sequence. 
+# 
+# 912/15/17 V0.1 RTK
+# 
+import argparse
+import re
+import sys
+import time
+import pandas as pd
+from pandas import DataFrame
+
+from rtk_io import call_exec_lines
+
+
+__version__ = "V0.1; RTK 12/15/17"
+
+
+BARLINE = '=' * 75
+
+# Help / explain details story
+def explain_story():
+    print(BARLINE)
+    print(__version__)
+    print(BARLINE)
+    print("Parses command line definitions feature table")
+    print()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Sequences to feature table')
+    parser.add_argument("-d", "--dfile",
+        help="Score definition (command line spec) filename\n"),
+    parser.add_argument("-s", "--seqfile",
+        help="Sequence file name")
+    parser.add_argument("-o", "--outfile",
+        help="Output file name")
+    parser.add_argument("-c", "--outcsv", action='store_true',
+        help="Output csv; i.e. values,separated,by,commas; Default is tabs")
+    parser.add_argument("-u", "--dump", action='store_true',
+        help="Dump summary of score defs")
+    parser.add_argument("-e", "--explain",
+        help="Print some info explaining use")
+
+    args = parser.parse_args()
+    if args.explain:
+        explain_story()
+        return
+
+    if not args.dfile:
+        print("No score definition file ... nothing to do!")
+        return
+
+    # Load definitions
+    s2f = Seqs2ftab(args.dfile)
+    if not s2f:
+        print("Sorry, problem loading score defs")
+        return
+    print("# Loaded score definitions")
+    if args.dump:
+        s2f.dump_comcalls(comcalls=True)
+    else:
+        s2f.dump_comcalls(comcalls=False, names=False)
+
+    if not args.seqfile:
+        print("No sequence file ... nothing more to do!")
+        return
+
+    # Process sequences
+    df = s2f.process_df(args.seqfile)
+    if df is None:
+        print("Sorry, problem getting feature table (DataFrame)")
+        return
+    print("Got feature table: {0}".format(df.shape))
+
+    # Set output 
+    if args.outfile:
+        try:
+            OFILE = open(args.outfile, 'w')
+        except IOError:
+            print("Failed to open output", args.outfile)
+            return 
+    else:
+        OFILE = sys.stdout
+
+    # Write output 
+    if args.outcsv:
+        sep = ','
+    else:
+        sep = '\t'
+    df.to_csv(path_or_buf=OFILE, sep=sep, index_label="SeqName")
+
+    # If new file, close and report
+    if OFILE is not sys.stdout:
+        OFILE.close()
+        print("New file: {0}".format(args.outfile))
+
+
+
+class Seqs2ftab:
+    """ Top level object for seqs-to-feature-table
+    
+    sdef = 'score' definition file; i.e. command line calling recipe to get features
+    sd_fmt = score def format
+    
+    """
+    def __init__(self, sdef, sd_fmt='txt', verb=True, sd_seq='PDNA', name=''):
+        self.sdef = sdef
+        self.sd_fmt = sd_fmt.lower()
+        self.verb = verb
+        self.name = name
+        # Set up score calls
+        self._init_comcalls_()
+        
+        
+    def __repr__(self):
+        ostring =  "Seqs2ftab object\n"
+        ostring += "NumCalls:  {0}\n".format(self.num_comcalls())
+        ostring += "TotalCols: {0}\n".format(self.num_totcols())
+        return ostring
+    
+    
+    # Private
+    # Parse command line definitions into comcall objects
+    def _init_comcalls_(self):
+        self.comcalls = []
+        if self.sd_fmt == 'txt':
+            self._parse_txt_sdef()
+        elif self.sd_fmt == 'json':
+            self._parse_raw_sdef()
+        else:
+            sham = "Unkown score def format: '{0}'".format(self.sd_fmt)
+            raise ValueError(sham)
+            
+  
+    # Private
+    # Parse score defs in (newer) text format
+    # Lines as: 
+    #    <command,arg[,arg]> <outcol>=<name> [<outcol>=<name>]
+    def _parse_txt_sdef(self):
+        with open(self.sdef) as INFILE:
+            # Regex for cigar segment parsing
+            COLOUT_REGEX = re.compile('(\d+)=(\w+)')
+            for line in INFILE:
+                line = line.strip()
+                # Ignore blank / comment lines
+                if not line or line.startswith('#'):
+                    continue
+                # Parse out parts
+                #    <command,arg[,arg]> <outcol>=<name> [<outcol>=<name>]
+                lparts = line.split()
+                comline = lparts[0].replace(',', ' ')
+                colouts = []
+                for part in lparts[1:]:
+                    m = COLOUT_REGEX.match(part)
+                    if not m:
+                        sham = "Misformed score def col=name: |{0}|\tLine: |{1}|".format(part,line)
+                        raise ValueError(sham)
+                    colouts.append(m.groups())
+                self.comcalls.append(ComCall(comline, colouts, rawdef=line))
+    
+
+    #    xxx TODO json format (someday?)
+    def _parse_json_sdef(self):
+        pass
+    
+    
+    def dump_comcalls(self, OFILE=sys.stdout, pref='# ', comcalls=False, names=False):
+        print(pref + "Seqs2ftab sequence score definitions", file=OFILE)
+        print(pref + "Number command calls: {0}".format(self.num_comcalls()), file=OFILE)
+        print(pref + "Total feature cols: {0}".format(self.num_totcols()), file=OFILE)
+        if comcalls:
+            for n,comcall in enumerate(self.comcalls):
+                print(pref + "{0:2d}\t{1}".format(n+1, comcall.call_string()), file=OFILE)
+                if names:
+                    print(pref + "\tN={0}:\t{1}".format(comcall.num_cols(), comcall.name_list()), file=OFILE)
+
+
+    # Number of command cols (features to be extracted)
+    def num_comcalls(self):
+        return len(self.comcalls)
+    
+    
+    # Number of total feature cols to be generated (calculated not counted)
+    def num_totcols(self):
+        tot = 0
+        for comcall in self.comcalls:
+            tot += comcall.num_cols()
+        return tot
+    
+    
+    def process_df(self, seqfile, verb=True):
+        """ Process all command calls with given sequence file
+        
+        seqfile = sequence filename to be processed
+        
+        returns DataFrame if all goes well
+        returns None if problem
+        """
+        stime = time.time()
+        for comcall in self.comcalls:
+            if not comcall.process(seqfile, verb=verb):
+                print('!' * 30 + ' PROBLEM ' + '!' * 30)
+                print(comcall)
+                print('!' * 30 + ' ABORTING ' + '!' * 30)
+                return None
+        
+        # Concatentate each comcall dataframe into single one
+        #    xxx TODO Error handling? try / except?
+        df = pd.concat([x.df for x in self.comcalls], axis=1)
+        if verb:
+            nrow, ncol = df.shape
+            print("# Total {0} rows X {1} cols".format(nrow, ncol))
+            print("# Total time {:0.1f} sec".format(time.time()-stime))
+        return df
+        
+    
+class ComCall:
+    """ Object to handle one command line call
+    
+    comcall = command line call (string)
+    colouts = column output mapping as list of (<col>,<name>) tuples
+    namecol = output column with name (i.e. sequence name)
+    rawdef = raw definition (e.g. score def file line); just for reporting
+    
+    """
+    def __init__(self, comcall, colouts, namecol=1, rawdef=None):
+        # Command call; Replace any commas with space
+        self.comcall = comcall.replace(',', ' ')
+        self.colouts = colouts
+        self.namecol = namecol
+        self.rawdef = rawdef
+        # Split tuples into two lists: cols (zero-base index), names
+        self.cols = []
+        self.names = []
+        for col, name in self.colouts:
+            self.cols.append(int(col)-1)
+            self.names.append(name)
+        self.col_max = max(self.cols)
+        # Check no duplicate cols or names specified
+        if len(self.cols) != len(set(self.cols)) or len(self.names) != len(set(self.names)):
+            if rawdef is not None:
+                sham = "Duplicated col / name specified: |{0}|".format(rawdef)
+            else:
+                sham = "Duplicated col / name specified: |{0}|".format(colouts)
+            raise ValueError(sham)      
+        # Name col from one-based to zero-base index
+        if self.namecol is not None:
+            self.namecol -= 1
+        
+        
+    def __repr__(self):
+        ostring =  "ComCall object\n"
+        ostring += "Call:  '{0}'\n".format(self.comcall)
+        ostring += "Num:   {0}\n".format(self.num_cols())
+        ostring += "Defs:  {0}\n".format(self.colouts)
+        return ostring
+    
+    
+    # Number of cols (features to be extracted from output lines)
+    def num_cols(self):
+        return len(self.cols)
+    
+
+    def call_string(self):
+        return self.comcall
+
+
+    def name_list(self):
+        return self.names
+
+
+    def process(self, seqfile, verb=True):
+        """ Call to process command line with given sequence file
+        
+        seqfile = sequence filename
+        
+        If all good, creates DataFrame with output data and returns True
+        Any problems, returns False
+        """
+        stime = time.time()
+        # Full command line = comcall part plus sequence filename
+        comargs = self.comcall + ' ' + seqfile
+        # Get list of output lines for call; 
+        # shell=True means shell gets spawned (e.g. so env vars good)
+        out = call_exec_lines(comargs, shell=True, verb=verb)
+        # False if problem (None)
+        if out is None:
+            return False
+        # Process output into dataframe (maybe cleaner way?)
+        # Row name list and dict of lists to collect col values
+        rownames = []
+        tdic = {}
+        for name in self.names:
+            tdic[name] = []
+        # Collect values from each row into separate lists
+        for r, row in enumerate(out):
+            parts = row.split()
+            if len(parts) <= self.col_max:
+                sham = "Output row too few cols; max {0}\t|{1}|".format(self.col_max, row)
+                raise ValueError(sham)    
+            if self.namecol is None:
+                rownames.append("row_{:05d}".format(r))
+            else:
+                rownames.append(parts[self.namecol])
+            for name, col in zip(self.names, self.cols):
+                tdic[name].append(parts[col])
+        self.df = DataFrame(index=rownames, data=tdic)
+        if verb:
+            nrow, ncol = self.df.shape
+            print("#  Data for {0} rows, {1} cols".format(nrow, ncol))
+            print("#  Time for call {:0.2f} ms".format(1000 * (time.time()-stime)))
+        return True
+
+
+
+
+
+
+#s2f = Seqs2ftab(SDFFILE, sd_fmt='txt')
+
+
+#y = s2f.process_df(SEQFILE)
+
+if __name__ == "__main__":
+    main()
+
+
